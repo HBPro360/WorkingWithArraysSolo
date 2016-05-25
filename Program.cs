c3db5fdd3382e861d5b7d2b7bcc94e8668f8f43e
HBPro360 <jphamm360@gmail.com>
2016-05-24 19:03:26 -0500

    WorkingWithArraysSolo

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithArraysSolo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a random generator
+            Random rnd = new Random();
+            //create a 10 element array of doubles
+            double[] nums = new double[10];
+            //loop through the length of the array
+            for (int i=0; i < nums.Length; i++)
+            {
+                //create a random number with a decimal or floating point numbers
+                double num = rnd.NextDouble();
+                //round to 2 decimal places
+                num = Math.Round(num, 3);
+                //put the rounded number into the array
+                nums[i] = num;
+                //write the position and the number to the console
+                Console.WriteLine(i.ToString() + ". " + nums[i]);
+            }
+            //wait
+            Console.WriteLine("Press any key to continue.....");
+            Console.ReadLine();
+        }
+    }
+}
